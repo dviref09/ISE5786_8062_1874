e6229c5efbbb2a468301e19bd5e25836cec99aad
Dvir Farkash <dviref09@gmail.com>
2026-03-12 23:04:02 +0200

    finishing Vector class and adding some settings for vscode

--- src/primitives/Point.java
+++ src/primitives/Point.java
@@ -1,94 +1,107 @@
-class Point {
-    /**
-     * The coordinates of the point.
-     */
-    private final Double3 _coordinates;
+package primitives;
 
-    /**
-     * A constant representing the axes origin.
-     */
-    private final Double3 ZERO = new Double3(0,0,0);
+/**
+ * A class for representing a point in 3D space.
+ * @author Amichai Feigelson
+ */
+public class Point {
+	/**
+	 * The coordinates of the point.
+	 */
+	protected final Double3 _coordinates;
 
-    /**
-     * Constructs a new point from the coordinates given in the parameters.
-     *
-     * @param x The x coordinate of the point.
-     * @param y The y coordinate of the point.
-     * @param z the z coordinate of the point.
-     */
-    public Point(double x, double y, double z) {
-        _coordinates = new Double3(x, y, z);
-    }
+	/**
+	 * A constant representing the axes origin.
+	 */
+	protected final Double3 ZERO = Double3.ZERO;
 
-    /**
-     * Constructs a new points from the coordinate given in the parameters.
-     *
-     * @param coordinates The cooridantes of the point.
-     */
-    public Point(Double3 coordinates) {
-        _coordinates = coordinates;
-    }
+	/**
+	 * Constructs a new point from the coordinates given in the parameters.
+	 * @param x The x coordinate of the point.
+	 * @param y The y coordinate of the point.
+	 * @param z the z coordinate of the point.
+	 */
+	public Point(double x, double y, double z) {
+		_coordinates = new Double3(x, y, z);
+	}
 
+	/**
+	 * Constructs a new points from the coordinate given in the parameters.
+	 * @param coordinates The cooridantes of the point.
+	 */
+	public Point(Double3 coordinates) {
+		_coordinates = coordinates;
+	}
 
-    /**
-     * Subtracts another point from our point.
-     * @param other The point to subtract from our point.
-     * @return The result vector.
-     * @throw IllegalArgumentException When the two points are equal.
-     */
-    public Vector subtract(Point other) {
-        if (other.equals(this)) {
-            throw new IllegalArgumentException("subtraction of point on itself results in zero vector");
-        }
-        return new Vector(this._coordinates.subtract(other._coordinates));
-    }
+	/**
+	 * Subtracts another point from our point.
+	 * @param other The point to subtract from our point.
+	 * @return The result vector.
+	 * @throw IllegalArgumentException When the two points are equal.
+	 */
+	public Vector subtract(Point other) {
+		if (other.equals(this)) {
+			throw new IllegalArgumentException("Subtraction of point on itself results in zero vector");
+		}
+		return new Vector(this._coordinates.subtract(other._coordinates));
+	}
 
-    /**
-     * Adds a vector to this point, returning a new point.
-     * @param vector The vector to add.
-     * @return A new point after adding the vector to this point.
-     */
-    public Point add(Vector vector) {
-        return new Point(_coordinates.add(vector.getCoordinates()));
-    }
+	/*
+	 * The following segment of code has been generated through github copilot with
+	 * the following prompt: "can you write this? make sure to keep DRY principle,
+	 * the same style of code, use Double3 when possible, and don't create temporary
+	 * variables. And create javadoc comments, simmilarly to what is already in the
+	 * code"
+	 */
 
-    /**
-     * Calculates the squared distance between this point and another point.
-     * @param other The other point.
-     * @return The squared distance between the two points.
-     */
-    public double distanceSquared(Point other) {
-        return _coordinates.subtract(other._coordinates).dotProduct(_coordinates.subtract(other._coordinates));
-    }
+	/**
+	 * Adds a vector to this point, returning a new point.
+	 * @param vector The vector to add.
+	 * @return A new point after adding the vector to this point.
+	 */
+	public Point add(Vector vector) {
+		return new Point(_coordinates.add(vector._coordinates));
+	}
 
-    /**
-     * Calculates the distance between this point and another point.
-     * @param other The other point.
-     * @return The distance between the two points.
-     */
-    public double distance(Point other) {
-        return Math.sqrt(distanceSquared(other));
-    }
+	/**
+	 * Calculates the squared distance between this point and another point.
+	 * @param other The other point.
+	 * @return The squared distance between the two points.
+	 */
+	public double distanceSquared(Point other) {
+		double distanceX = this._coordinates._d1() - other._coordinates._d1();
+		double distanceY = this._coordinates._d2() - other._coordinates._d2();
+		double distanceZ = this._coordinates._d3() - other._coordinates._d3();
+		return distanceX * distanceX + distanceY * distanceY + distanceZ * distanceZ;
+	}
 
+	/**
+	 * Calculates the distance between this point and another point.
+	 * @param other The other point.
+	 * @return The distance between the two points.
+	 */
+	public double distance(Point other) {
+		return Math.sqrt(distanceSquared(other));
+	}
 
-    @Override
-    public boolean equals(Object other) {
-        if (this == other) {
-            return true;
-        }
-        if (object == null || getClass() != object.getClass()) {
-            return false;
-        }
-        return _coordinates.equals(((Point) object)._coordinates);
-    }
+	@Override
+	public boolean equals(Object other) {
+		if (this == other) {
+			return true;
+		}
+		if (other == null || getClass() != other.getClass()) {
+			return false;
+		}
+		return _coordinates.equals(((Point) other)._coordinates);
+	}
 
-    @Override
-    public String toString() {
-        return "Point: " + _coordinates;
-    }
+	@Override
+	public String toString() {
+		return "Point: " + _coordinates;
+	}
 
-    @Override
-    public int hashCode() {
-        return _coordinates.hashCode());
-    }
+	@Override
+	public int hashCode() {
+		return _coordinates.hashCode();
+	}
 }
---
+++ src/primitives/Vector.java
@@ -0,0 +1,126 @@
+package primitives;
+
+/**
+ * A class for representing a vector in 3D space.
+ * @author Dvir Farkash
+ */
+public class Vector extends Point {
+	/**
+	 * Constructs a new vector with the specified coordinates.
+	 * @param x the x coordinate of the vector
+	 * @param y the y coordinate of the vector
+	 * @param z the z coordinate of the vector
+	 */
+	public Vector(double x, double y, double z) {
+		super(x, y, z);
+		if (_coordinates.equals(ZERO)) {
+			throw new IllegalArgumentException("Zero vector is not allowed");
+		}
+	}
+
+	/**
+	 * Constructs a new vector with the specified coordinates.
+	 * @param coordinates The coordinates of the vector
+	 */
+	public Vector(Double3 coordinates) {
+		super(coordinates);
+		if (_coordinates.equals(ZERO)) {
+			throw new IllegalArgumentException("Zero vector is not allowed");
+		}
+	}
+	/*
+	 * The following segment of code has been generated through github copilot with
+	 * the following prompt: "תכין לי את רשימת המתודות שכתובה בתמונה, אם יש מתודה
+	 * שאתה לא בטוח מה היא אמורה לעשות תשאל אותי. תייצר גם הערת javadoc, תקפיד על כך
+	 * שסגנון ההערכות והקוד יהיה דומה לשאר הקוד וההערות."
+	 */
+
+	/**
+	 * Adds another vector to this vector.
+	 * @param other The vector to add.
+	 * @return A new vector that is the sum of the two vectors.
+	 * @throws IllegalArgumentException When the result is the zero vector.
+	 */
+	public Vector add(Vector other) {
+		return new Vector(_coordinates.add(other._coordinates));
+	}
+
+	/**
+	 * Scales this vector by a scalar value.
+	 * @param factor The factor of multiplication.
+	 * @return A new vector that is the result of the scaling.
+	 * @throws IllegalArgumentException When the factor is zero.
+	 */
+	public Vector scale(double factor) {
+		return new Vector(_coordinates.scale(factor));
+	}
+
+	/**
+	 * Calculates the dot product of this vector with another vector.
+	 * @param other The other vector.
+	 * @return The dot product of the two vectors.
+	 */
+	public double dotProduct(Vector other) {
+		return _coordinates._d1() * other._coordinates._d1() + _coordinates._d2() * other._coordinates._d2()
+				+ _coordinates._d3() * other._coordinates._d3();
+	}
+
+	/**
+	 * Calculates the cross product of this vector with another vector.
+	 * @param other The other vector.
+	 * @return A new vector that is the cross product of the two vectors.
+	 * @throws IllegalArgumentException When the two vectors are parallel.
+	 */
+	public Vector crossProduct(Vector other) {
+		double otherX = other._coordinates._d1(), otherY = other._coordinates._d2(), otherZ = other._coordinates._d3();
+		double thisX = _coordinates._d1(), thisY = _coordinates._d2(), thisZ = _coordinates._d3();
+		double resultX = thisY * otherZ - thisZ * otherY;
+		double resultY = thisZ * otherX - thisX * otherZ;
+		double resultZ = thisX * otherY - thisY * otherX;
+		return new Vector(resultX, resultY, resultZ);
+	}
+
+	/**
+	 * Calculates the squared length of this vector.
+	 * @return The squared length of the vector.
+	 */
+	public double lengthSquared() {
+		return dotProduct(this);
+	}
+
+	/**
+	 * Calculates the length of this vector.
+	 * @return The length of the vector.
+	 */
+	public double length() {
+		return Math.sqrt(lengthSquared());
+	}
+
+	/**
+	 * Returns a new normalized (unit) vector in the same direction as this vector.
+	 * @return A new unit vector in the direction of this vector.
+	 */
+	public Vector normalize() {
+		return scale(1 / length());
+	}
+
+	/*
+	 * The following segment of code has been generated through github copilot with
+	 * the following prompt: "עכשיו תדרוס את מתודות equlas, tostring, hashcode בצורה
+	 * שתתאים לסגנון הקוד כאן בפרוייקט, ובדומה לפונקציות האלו בpoint, ותקפיד על dry"
+	 */
+	@Override
+	public boolean equals(Object other) {
+		return super.equals(other);
+	}
+
+	@Override
+	public String toString() {
+		return "Vector: " + _coordinates;
+	}
+
+	@Override
+	public int hashCode() {
+		return super.hashCode();
+	}
+}
