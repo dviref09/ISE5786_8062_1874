e5ac7ce40573d7952a9d93058ae69c759097e342
Amichai Feigelson <amichai.feigelson@gmail.com>
2026-03-16 14:29:32 +0200

    started sphere class

---
+++ src/geometries/impl/Sphere.java
@@ -0,0 +1,15 @@
+package geometries.impl;
+
+import geometries.api.Geometry;
+import primitives.Point;
+
+/**
+ * Class representing a shphere in 3D space.
+ * @author Amichai Feigelson
+ */
+public class Sphere extends RadialGeometry {
+	/**
+	 * The center point of the sphere.
+	 */
+	private final Point _center;
+}
