f8a6fd4d35cd9ac9a69bd825783fd5f7479df7c1
Dvir Farkash <dviref09@gmail.com>
2026-03-16 15:56:17 +0200

    finishing sphere class and starting tube class

--- src/geometries/impl/Sphere.java
+++ src/geometries/impl/Sphere.java
@@ -12,4 +12,26 @@ public class Sphere extends RadialGeometry {
 	 * The center point of the sphere.
 	 */
 	private final Point _center;
+
+	@Override
+	public Vector getNormal(Point point) {
+		return null;
+	}
+
+	@Override
+	public boolean equals(Object other) {
+		return super.equals(other) && _center.equals((Sphere) other._center);
+	}
+
+	@Override
+	public String toString() {
+		return "Sphere:/n/t" + 
+				"Center: " + _center + "/n/t" + 
+				"Radius: " + _radius;
+	}
+
+	@Override
+	public int hashCode() {
+		Objects.hash(super.hashCode(), _center);
+	}
 }
