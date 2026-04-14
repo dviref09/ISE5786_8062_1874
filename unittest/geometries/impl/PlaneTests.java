71a86c619ebab11ce26c4959af1d4a1e625bcf94
Dvir Farkash <dviref09@gmail.com>
2026-04-14 13:24:51 +0300

    finishing plane tests

--- unittest/geometries/impl/PlaneTests.java
+++ unittest/geometries/impl/PlaneTests.java
@@ -1,8 +1,11 @@
 package geometries.impl;
 
 import org.junit.jupiter.api.Test;
+import primitives.Point;
+import primitives.Vector;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 /**
  * Unit test class for {@link geometries.impl.Plane}.
@@ -26,7 +29,7 @@ public class PlaneTests {
 	private final static Plane plane4 = new Plane(point1, point1, point1);
 	private final static Plane plane5 = new Plane(point1, new Point(0, 0, 2), new Point(0, 0, 3));*/
 	private final static Vector orthogonalVector1 = point1.subtract(point2);
-	private final static Vector orthogonalVector2 = point1.substract(point3);
+	private final static Vector orthogonalVector2 = point1.subtract(point3);
 
 	private final static String LENGTH_FAILURE_MESSAGE = "The normal should be of length 1.";
 	private final static String DIRECTION_FAILURE_MESSAGE = "The dot product of the normal and a vector made from 2 points in the plane should be 0.";
@@ -40,14 +43,14 @@ public class PlaneTests {
 	void testGetNormal() {
 		// ============ Equivalence Partitions Tests ==============
 		// EP01: Getting a normal for point on the plane
-		Vector result = plane1.getNormal(point4);
+		Vector result = plane2.getNormal(point4);
 		assertEquals(1, result.length(), DELTA, LENGTH_FAILURE_MESSAGE);
 		assertEquals(0, result.dotProduct(orthogonalVector1), DELTA, DIRECTION_FAILURE_MESSAGE);
 		assertEquals(0, result.dotProduct(orthogonalVector2), DELTA, DIRECTION_FAILURE_MESSAGE);
 
 		// =============== Boundary Values Tests ==================
 		// BV01: Getting a normal for the point used in the plane constructor
-		result = plane1.getNormal(point1);
+		result = plane2.getNormal(point1);
 		assertEquals(1, result.length(), DELTA, LENGTH_FAILURE_MESSAGE);
 		assertEquals(0, result.dotProduct(orthogonalVector1), DELTA, DIRECTION_FAILURE_MESSAGE);
 		assertEquals(0, result.dotProduct(orthogonalVector2), DELTA, DIRECTION_FAILURE_MESSAGE);
