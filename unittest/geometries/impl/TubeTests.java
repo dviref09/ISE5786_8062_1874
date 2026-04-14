1babac60b93c57ab1cd901e73a340e924bef79ab
Dvir Farkash <dviref09@gmail.com>
2026-04-14 22:47:19 +0300

    added TubeTests class

---
+++ unittest/geometries/impl/TubeTests.java
@@ -0,0 +1,14 @@
+package geometries.impl;
+
+import org.junit.jupiter.api.Test;
+import primitives.Point;
+import primitives.Vector;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+/**
+ * Unit test class for {@link geometries.impl.Tube}.
+ * @author Amichai Feigelson
+ */
+public class TubeTests {
+
+}
