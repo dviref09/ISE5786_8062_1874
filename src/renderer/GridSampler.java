d0d134479cfdfdc6e0c965b27185154df8b13d58
Amichai Feigelson <amichai.feigelson@gmail.com>
2026-06-16 13:34:07 +0300

    Created Blackboard Added Sampler interface Created GridSampler, no javadoc

--- src/renderer/GridSampler.java
+++ src/renderer/GridSampler.java
@@ -6,5 +6,15 @@ import primitives.Point;
 
 public class GridSampler implements Sampler {
 	public List<List<Double>> sameplePoints(int nX, int nY) {
+		List<List<Double>> points = new java.util.ArrayList<>();
+		for (int i = -nX / 2; i < nX / 2; i++) {
+			for (int j = -nY / 2; j < nY /2; j++) {
+				List<Double> point = new java.util.ArrayList<>();
+				point.add((i + 0.5) / nX);
+				point.add((j + 0.5) / nY);
+				points.add(point);
+			}
+		}
+		return points;
 	}
 }
